e72c29725f1ef1e476c7a220fc76e39c5aa98ac2
Anders Hovmöller <anders.hovmoller@dryft.se>
2025-12-31 17:09:26 +0100

    Initial Commit

---
+++ TadaTests/TadaTests.swift
@@ -0,0 +1,16 @@
+//
+//  TadaTests.swift
+//  TadaTests
+//
+//  Created by Anders Hovmöller on 2025-12-31.
+//
+
+import Testing
+
+struct TadaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
